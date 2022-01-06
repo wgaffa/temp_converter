fcc5736ed63660748acf875a4d8b8d7994b9e9ac
Patrik Maunus <subscription@skriptladan.se>
2022-01-06 17:47:27 +0100

    Add quickcheck tests for converters

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+#[derive(Debug, Clone, PartialEq)]
 pub struct Celsius(f64);
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Fahrenheit(f64);
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Kelvin(f64);
 
 macro_rules! impl_f64_conv {
@@ -33,3 +38,47 @@ impl_conv!(Kelvin, Celsius => |x: f64| x - 273.15);
 impl_conv!(Kelvin, Fahrenheit => |x: f64| x * 9.0/5.0 - 459.67);
 impl_conv!(Fahrenheit, Celsius => |x: f64| (x - 32.0) * 5.0/9.0);
 impl_conv!(Fahrenheit, Kelvin => |x: f64| (x + 459.67) * 5.0/9.0);
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use quickcheck::{quickcheck, Arbitrary, Gen};
+    use float_cmp::approx_eq;
+
+    macro_rules! arbitrary_two_point_precision {
+        ($($i:ident),*) => {
+            $(
+                impl Arbitrary for $i {
+                    fn arbitrary(gen: &mut Gen) -> Self {
+                        let num = (f64::arbitrary(gen) * 100.0).round() / 100.0;
+                        $i(num)
+                    }
+                }
+            )*
+        }
+    }
+
+    macro_rules! quick_temp {
+        ($name:ident , $base:ident, $target:ident) => {
+            quickcheck! {
+                fn $name(input: $base) -> bool {
+                    let actual = input.clone();
+                    let target: $target = input.into();
+                    let expected: $base = target.into();
+
+                    approx_eq!(f64, actual.0, expected.0, ulps = 2, epsilon = 0.01)
+                }
+            }
+        }
+    }
+
+    arbitrary_two_point_precision!(Celsius, Fahrenheit, Kelvin);
+
+    quick_temp!(celsius_fahrenheit, Celsius, Fahrenheit);
+    quick_temp!(celsius_kelvin, Celsius, Kelvin);
+    quick_temp!(fahrenheit_celsius, Fahrenheit, Celsius);
+    quick_temp!(fahrenheit_kelvin, Fahrenheit, Kelvin);
+    quick_temp!(kelvin_celsius, Kelvin, Celsius);
+    quick_temp!(kelvin_fahrenheit, Kelvin, Fahrenheit);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use temp_converter::{Celsius, Fahrenheit};
+
 fn main() {
     println!("Hello, world!");
+
+    let celsius = Celsius::from(1.2357468219272696e308);
+    println!("{:?}", celsius);
+
+    let fahr = Fahrenheit::from(celsius);
+    println!("{:?}", fahr);
+
+    let celsius = Celsius::from(fahr);
+    println!("{:?}", celsius);
 }
