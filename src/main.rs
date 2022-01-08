232c9ddc85631462134a4f17ec4b5b00dd4bcb5d
Patrik Maunus <subscription@skriptladan.se>
2022-01-08 15:01:35 +0100

    Add temperature argument
    A temperature is validated using str.parse method

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 #![feature(bool_to_option)]
 
-use clap::{crate_name, crate_version, App, Arg};
+use clap::{crate_version, App, Arg};
 
 use temp_converter::{Celsius, Fahrenheit};
-
-fn unit_validator(input: &str) -> Result<(), &'static str> {
-    let uppercase = input.to_uppercase();
-    matches!(uppercase.chars().nth(0), Some('C') | Some('F') | Some('K'))
-        .then_some(())
-        .ok_or("Valid unit types are (C)elsius, (F)ahrenheit and (K)elvin")
-}
 
 fn main() {
     let app = App::new("Temperature Converter")
         .version(crate_version!())
-        .arg(
+        .args(&[
+            Arg::new("temperature")
+                .required(true)
+                .validator(|x| match x.parse::<f64>() {
+                    Ok(_) => Ok(()),
+                    Err(_) => Err("must be a floating point number with dot '.' as delimiter"),
+                })
+                .help("The base temperature to convert"),
             Arg::new("base")
                 .short('b')
                 .takes_value(true)
@@ -22,8 +22,6 @@ fn main() {
                 .possible_values(["C", "F", "K"])
                 .ignore_case(true)
                 .help("Base unit to convert"),
-        )
-        .arg(
             Arg::new("target")
                 .short('t')
                 .takes_value(true)
@@ -31,6 +29,6 @@ fn main() {
                 .possible_values(["C", "F", "K"])
                 .ignore_case(true)
                 .help("Target unit to convert to"),
-        )
+        ])
         .get_matches();
 }
